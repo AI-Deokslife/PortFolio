d70d65ead33672a2fccee29f79798d1a9ce09c6c
덕쌤 <deokslife@gmail.com>
2025-08-08 04:31:42 +0900

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -81,7 +81,7 @@ export default function Header({ appCount = 0, onSettingsClick }: HeaderProps) {
     <HeaderContainer>
       <HeaderContent>
         <Brand>
-          <Title>EunDeok&apos;s AI Vibe Coding Portfolio</Titleㅁ
+          <Title>EunDeok&apos;s AI Vibe Coding Portfolio</Title>
         </Brand>
         <Stats>
           <StatItem>
