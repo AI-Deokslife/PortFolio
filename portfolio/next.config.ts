146c921c00cca8d7e7afc7bfeb38c5bca0a4cb70
덕쌤 <deokslife@gmail.com>
2025-08-09 21:02:25 +0900

    Update next.config.ts

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const config: NextConfig = {
-   api: {
-    bodyParser: {
-      sizeLimit: '50mb', // 원하는 크기로 설정 (예: 10MB)
-    },
-   },
-};
+const nextConfig: NextConfig = {
+  /* config options here */
+   };
 
-export default config;
+export default nextConfig;
